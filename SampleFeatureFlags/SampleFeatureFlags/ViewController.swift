628572035ba7e9bfe277119dfa432752f0cb5386
Oleg <goaIKPI@gmail.com>
2021-04-17 17:40:32 +0300

    feat(sample): init project

---
+++ SampleFeatureFlags/SampleFeatureFlags/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleFeatureFlags
+//
+//  Created by mac on 17.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
